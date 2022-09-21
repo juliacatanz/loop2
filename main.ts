87ed7713f76e568f94b3749fb1e1e83218713f3e
juliacatanz <113040686+juliacatanz@users.noreply.github.com>
2022-09-21 11:01:55 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        Jimmy_steve.change(LedSpriteProperty.X, 1)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    Jimmy_steve.change(LedSpriteProperty.Y, -1)
+})
+let Jimmy_steve: game.LedSprite = null
+basic.showIcon(IconNames.Heart)
+Jimmy_steve = game.createSprite(0, 0)
+loops.everyInterval(500, function () {
+	
+})
+basic.forever(function () {
+	
+})
