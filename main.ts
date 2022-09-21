05b861af37fc5955a05d6475e66f9d8eb281bf6b
juliacatanz <113040686+juliacatanz@users.noreply.github.com>
2022-09-21 11:15:34 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,24 @@
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index <= 4; index++) {
         Jimmy_steve.change(LedSpriteProperty.X, 1)
+        basic.pause(100)
+    }
+    for (let index = 0; index <= 4; index++) {
+        Jimmy_steve.change(LedSpriteProperty.Y, 1)
+        basic.pause(100)
+    }
+    for (let index = 0; index <= 4; index++) {
+        Jimmy_steve.change(LedSpriteProperty.X, -1)
+        basic.pause(100)
+    }
+    for (let index = 0; index <= 4; index++) {
+        Jimmy_steve.change(LedSpriteProperty.Y, -1)
+        basic.pause(100)
     }
 })
 let Jimmy_steve: game.LedSprite = null
 basic.showIcon(IconNames.Heart)
-Jimmy_steve = game.createSprite(3, 3)
-loops.everyInterval(500, function () {
-	
-})
+Jimmy_steve = game.createSprite(4, 4)
 basic.forever(function () {
 	
 })
